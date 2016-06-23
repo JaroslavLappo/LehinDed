ec2ca6a01f41888a2126f5ffdc67e67f89e169f6
JaroslavLappo <jar102938@gmail.com>
2016-06-23 19:51:47 +0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,21 @@
+/*var port = process.env.PORT || 8080;
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+http.listen(port, function () {
+    console.log('listening on port: ' + port.toString());
+});*/
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+  console.log(request.socket.address());
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("This is leha");
+  response.end();
+}).listen(8080);
